32f79424eabf90e42087f4778c2e562928901efa
신희진 <9902102@naver.com>
2024-01-26 14:10:03 +0900

    시작

---
+++ src/main/java/spring/myapp_v1/MyappV1Application.java
@@ -0,0 +1,13 @@
+package spring.myapp_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/spring/myapp_v1/MyappV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.myapp_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
