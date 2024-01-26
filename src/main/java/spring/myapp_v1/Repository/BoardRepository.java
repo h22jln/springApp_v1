f93fa6d4e0c52889b80ce8ecd31183f7b0356ef4
신희진 <9902102@naver.com>
2024-01-26 19:02:03 +0900

    일단 기능 구현 끝

--- src/main/java/spring/myapp_v1/Repository/BoardRepository.java
+++ src/main/java/spring/myapp_v1/Repository/BoardRepository.java
@@ -2,7 +2,11 @@ package spring.myapp_v1.Repository;
 
 import spring.myapp_v1.Domain.Board;
 
+import java.util.List;
+
 
 public interface BoardRepository {
     int save(Board board);
+
+    List<Board> selectBoards();
 }
--- src/main/java/spring/myapp_v1/Repository/BoardRepositoryImpl.java
+++ src/main/java/spring/myapp_v1/Repository/BoardRepositoryImpl.java
@@ -2,9 +2,12 @@ package spring.myapp_v1.Repository;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import spring.myapp_v1.Domain.Board;
 
 import javax.sql.DataSource;
+import java.util.List;
+
 @Slf4j
 public class BoardRepositoryImpl implements BoardRepository {
     private final JdbcTemplate template;
@@ -15,8 +18,27 @@ public class BoardRepositoryImpl implements BoardRepository {
 
     @Override
     public int save(Board board) {
-        String sql = "insert into board(name,content,password,ckPrivate) values(?, ?,?,?)";
+        String sql = "insert into board(name,content,password,ckPrivate) values(?,?,?,?)";
         int update = template.update(sql, board.getName(), board.getContent(), board.getPassword(), board.isCkPrivate());
         return update;
+    }
+
+    @Override
+    public List<Board> selectBoards() {
+        String sql = "SELECT * FROM board";
+        return template.query(sql, boardRowMapper());
+    }
+
+    private RowMapper<Board> boardRowMapper() {
+        return (rs, rowNum) -> {
+            Board board = new Board(
+                    rs.getString("name"),
+                    rs.getString("content"),
+                    rs.getString("password")
+            );
+            board.setCreated_at(rs.getString("created_at"));
+            board.setCkPrivate(rs.getBoolean("ckPrivate"));
+            return board;
+        };
     }
 }
